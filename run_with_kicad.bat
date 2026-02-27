fce99998e425bd017db68cbd65dfaa41ef9b8807
DanielDongRepo <3377027295@qq.com>
2026-02-27 23:39:17 +0800

    Update run_with_kicad.bat

--- run_with_kicad.bat
+++ run_with_kicad.bat
@@ -1,15 +1,16 @@
 @echo off
-REM 确保 KiCad bin 在 PATH 最前面（避免其他版本干扰）
-set PATH=E:\kicad\bin;%PATH%
+REM 确保 KiCad bin 在 PATH 最前面（避免其他版本干扰）这里改为自己的kicad的bin路径
+set PATH=C:\kicad\bin;%PATH%
 
-REM 设置 Python 模块路径
-set PYTHONPATH=E:\kicad\bin\Lib\site-packages;%PYTHONPATH%
+REM 设置 Python 模块路径。假设kicad安装在全部c盘，项目也在c盘，虚拟环境在项目目录下
+set PYTHONPATH=C:\kicad\bin\Lib\site-packages;%PYTHONPATH%
 
-REM 激活虚拟环境
-call F:\kicad-ai-inspector\kicad-ai-env\Scripts\activate.bat
+REM 激活虚拟环境()
+call C:\kicad-ai-inspector\kicad-ai-env\Scripts\activate.bat
 
 REM 进入项目目录
-cd /d F:\kicad-ai-inspector
+cd /d C:\kicad-ai-inspector
 
 REM 启动交互式命令行
+
 cmd /k
